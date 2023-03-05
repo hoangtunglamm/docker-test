926c0d324360620f7f6a1fd64cf42cc802936f8f
lamht <lamht.iist@gmail.com>
2023-03-05 16:58:53 +0700

    1st commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit  } from '@angular/core';
+import { ActorService } from 'src/services/actor.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  constructor(public actorService: ActorService) {}
+  data: any;
+  ngOnInit(): void {
+    console.log('123');
+    this.actorService.getActor().subscribe(res => {
+      this.data = res;
+    });
+  }
   title = 'FE';
+
 }
