d5ffb0f3b3671528a484b0bbd8e4929b3bfd45c6
lamht <lamht.iist@gmail.com>
2023-03-31 23:01:38 +0700

    len

--- src/services/actor.service.ts
+++ src/services/actor.service.ts
@@ -9,6 +9,6 @@ export class ActorService {
   constructor(private http: HttpClient) { }
 
   getActor(){
-    return this.http.get('http://localhost:3000/test');
+    return this.http.get('http://34.87.24.85:4201/test');
   }
 }
